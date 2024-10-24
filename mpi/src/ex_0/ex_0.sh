6bee9b4c0067dbc6fb9e31c1edd5918b855834aa
acyang <2888344+acyang@users.noreply.github.com>
2024-10-24 13:57:30 +0800

    Add files via upload

---
+++ mpi/src/ex_0/ex_0.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+#SBATCH --account=GOV113078           # (-A) iService Project ID
+#SBATCH --job-name=python-mpi         # (-J) Job name
+#SBATCH --partition=development       # (-p) Slurm partition
+#SBATCH --nodes=2                     # (-N) Maximum number of nodes to be allocated
+#SBATCH --cpus-per-task=1             # (-c) Number of cores per MPI task
+#SBATCH --ntasks-per-node=2         # Maximum number of tasks on each node
+#SBATCH --time=00:30:00               # (-t) Wall time limit (days-hrs:min:sec)
+#SBATCH --output=job-%j.out           # (-o) Path to the standard output file
+#SBATCH --error=job-%j.err            # (-e) Path to the standard error filei
+
+module purge
+module load gcc/8.5.0
+module load openmpi/5.0.2
+module load tools/miniconda3
+
+eval "$(conda shell.bash hook)"
+
+conda activate python-mpi
+mpiexec -n $SLURM_NTASKS python3 ex_0.py
+
